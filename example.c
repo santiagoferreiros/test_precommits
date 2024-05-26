9203128f62c820ef57c6dab215e631cb7432a837
santiagoferreiros <46604718+santiagoferreiros@users.noreply.github.com>
2024-05-26 00:25:23 -0300

    Fix to pass checks

--- example.c
+++ example.c
@@ -2,6 +2,6 @@
 int main() {
   int a = 0;
   int b = ++a;
-  printf("Value: %d", a);
+  printf("Value: %d", b);
   return 0;
 }
--- exmple2.c
+++ exmple2.c
@@ -2,6 +2,6 @@
 int main() {
   int a = 11;
   int b = ++a;
-  printf("Value: %d", a);
+  printf("Value: %d", b);
   return 0;
 }
