2518e68a2d44b0144929986d39f19a35e945b8d0
santiagoferreiros <46604718+santiagoferreiros@users.noreply.github.com>
2024-05-26 23:44:38 -0300

    add ls

--- TP1/tests/run_tests_tp_1.sh
+++ TP1/tests/run_tests_tp_1.sh
@@ -4,10 +4,10 @@
 gcc TP1/src/main.c -o TP1/bin/tp1
 
 # Ejecutar pruebas
-for input_file in test_files/input_test*.txt; do
+for input_file in TP1/tests/test_*.txt; do
     test_name=$(basename ${input_file} .txt)
     output_file="output_${test_name}.txt"
-    expected_output="expected_outputs/expected_output_${test_name}.txt"
+    expected_output="TP1/tests/expected_outputs/expected_output_${test_name}.txt"
 
     # Ejecutar el programa con rutas de archivo de entrada y salida
     TP1/bin/tp1 ${input_file} ${output_file}
