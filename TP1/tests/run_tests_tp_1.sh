306728c7ad8edf66e387416067db837ad4b3983e
santiagoferreiros <46604718+santiagoferreiros@users.noreply.github.com>
2024-05-27 00:41:56 -0300

    asdfdsf

--- TP1/tests/run_tests_tp_1.sh
+++ TP1/tests/run_tests_tp_1.sh
@@ -28,9 +28,9 @@ for input_file in TP1/tests/test_*.txt; do
     if colordiff -y ${output_file} ${expected_output} > /dev/null; then
         echo "Resultado ${test_name}: PASS"
     else
-        echo -e "Resultado ${test_name}: FAIL \n"
-        echo -e "\nDiferencias indicadas en colores. Referencias de colores: \n\n  *Verde: Líneas faltantes en salida actual \n  *Rojo: Líneas adicionales en salida actual \n  *Celeste: Líneas con diferencias\n"
-        echo -e "\nSalida de ejecución actual ${test_name} \t\t\t\t\tSalida esperada ${test_name}\n"
+        echo -e "Resultado ${test_name}: FAIL"
+        echo -e "\nDiferencias indicadas en colores. Referencias de colores: \n\n  *Verde: Líneas faltantes en salida actual \n  *Rojo: Líneas adicionales en salida actual \n  *Celeste: Líneas con diferencias entre salida actual y esperada"
+        echo -e "\nSalida de ejecución actual ${test_name} \t\t\t\tSalida esperada ${test_name}\n"
         colordiff -y ${output_file} ${expected_output}
     fi
 done
